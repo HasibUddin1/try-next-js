c173a99471acc00dede8845461738e03afff8214
IconicLuck <uhasib959@gmail.com>
2023-07-26 20:40:08 +0600

    created dynamic route for every blog

--- src/app/(with-layout)/blogs/page.js
+++ src/app/(with-layout)/blogs/page.js
@@ -1,12 +1,39 @@
+import Link from "next/link";
+
 export const metadata = {
     title: 'Next Hero | Blogs',
     description: 'Next Hero',
 }
 
+const blogs = [
+    {
+        id: 1,
+        year: 2016,
+        title: 'Title 1'
+    },
+    {
+        id: 2,
+        year: 2018,
+        title: 'Title 2'
+    },
+    {
+        id: 3,
+        year: 2015,
+        title: 'Title 3'
+    },
+    {
+        id: 4,
+        year: 2023,
+        title: 'Title 4'
+    }
+]
+
 const BlogsPage = () => {
     return (
         <div>
-            <h1>This is Blogs Page</h1>
+            {
+                blogs.map(SingleBlog => <Link href={`/blogs/${SingleBlog.year}/${SingleBlog.id}`} key={SingleBlog.id}>{SingleBlog.title}</Link>)
+            }
         </div>
     );
 };
